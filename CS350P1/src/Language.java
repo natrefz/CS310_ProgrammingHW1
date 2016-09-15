91b94a2aa20d8482302653c5663a9167273a1dde
Jared Bartrug <jerad4@gmail.com>
2016-09-15 14:37:01 -0400

    Set up theory for other languages needs tested and looked at for mistakes.

--- CS350P1/src/Language.java
+++ CS350P1/src/Language.java
@@ -33,15 +33,18 @@ public class Language{
 			tok.getNextToken();		
 			if(S1()){
 				if(tok.getNextToken()=='c'){
+					tok.getNextToken();
 					if(B1()){
 						return true;
 					}
 				}
 			}
+			else{
+				return false;
+			}
 		}
 		if (tok.getCurrentToken() == 'b')
 		{
-			tok.getNextToken();
 			return true;
 		}
 		if(A1()){
@@ -75,4 +78,107 @@ public class Language{
 		}
 		return false;
 	}
+	public boolean assign(){
+		
+		if(ID()){
+			tok.getNextToken();
+			if(tok.getCurrentToken()=='='){
+				tok.getNextToken();
+				if(expr()){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public boolean expr(){
+		if(digit()){
+			if(tok.getNextToken()=='+'){
+				if(expr()){
+					return true;
+				}
+			}
+			if(tok.getNextToken()=='-'){
+				if(expr()){
+					return true;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	public boolean ID(){
+		if(tok.getCurrentToken() =='a'||tok.getCurrentToken()=='b'){
+			return true;
+		}
+		return false;
+	}
+	public boolean digit(){
+		int digit = Character.getNumericValue(tok.getCurrentToken());
+		if(digit>=0||digit<10){
+			return true;
+		}
+		return false;
+	}
+	public boolean A2(){
+		if(tok.getCurrentToken()=='a'){
+			tok.getNextToken();
+			if(A2()){
+				if(tok.getNextToken()=='c'){
+					return true;
+				}
+			}
+		}
+		if(B2()){
+			return true;
+		}
+		return false;
+	}
+	public boolean B2(){
+		if(tok.getCurrentToken()=='b'){
+			tok.getNextToken();
+			if(B2()){
+				if(tok.getNextToken()=='c'){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public boolean S4(){
+		if(A4()){
+			if(tok.getNextToken()=='a'){
+				tok.getNextToken();
+				if(B4()){
+					if(tok.getNextToken()=='a'){
+						return true;
+					}
+				}	
+			}
+		}
+		return false;
+	}
+	public boolean A4(){
+		if(A4()){
+			if(tok.getNextToken()=='b'){
+				return true;
+			}
+		}
+		if(tok.getCurrentToken()=='b'){
+			return true;
+		}
+		return false;
+	}
+	public boolean B4(){
+		if(tok.getCurrentToken()=='a'){
+			if(B4()){
+				return true;
+			}
+		}
+		if(tok.getCurrentToken()=='a'){
+			return true;
+		}
+		return false;
+	}
 }
+
